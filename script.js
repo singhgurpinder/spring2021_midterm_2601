5cc701abc4e92b78c1b13116064f19c0212dc34c
Plasnid <stuart@sackler.ca>
2021-06-14 14:29:43 -0400

    initial file upload

---
+++ script.js
@@ -0,0 +1,56 @@
+/**
+ * ! Spring 2021 Coding for the Web 1 Midterm - 52 points total
+ * * Only answer 1 questions from each section.
+ * 
+ * ! Section 1: numbers - 5 points
+ * * Please answer either a or b from this section
+ */
+
+/**
+ * * 1a. instantiate and name the following variables
+ * *       i. a variable representing the age of a pet  
+ * *      ii. a variable representing the number of people in a family
+ * *     iii. a variable representing how full a gas tank is
+ */
+
+/**
+ * * 1b. Please correct the following code and console log a description of your correction.
+ */
+
+//let hoursin theyear === "24"*"365"
+
+/**
+ * ! Section 2: Strings and template literals - 15 points
+ */
+
+/**
+ * * 2. Please create the variables listed, then add them to the sentence below using template literals.  Console log the result and add it to the page in an h1 tag.
+ *       
+ *        i. person
+ *       ii. an adjective(describes a person place or thing) 
+ *      iii. a place
+ *       iv. a food
+ *        v. a plural noun(plural of a noun like cats.  A noun is a person, place or thing)
+ *       vi. a plural noun(plural of a noun like cats.  A noun is a person, place or thing)
+ */
+
+let lib = `I just got back from a pizza party with __i__. Can you believe we got to eat __ii__ pizza in __iii__?! Everyone got to choose their own toppings. I made '__iv__ and __v__' pizza, which is my favorite! They even stuffed the crust with __vi__.`
+
+/**
+ * ! Section 3: Arrays - 20 points
+ * 
+ * * Convert the list below into an array, then add 4 more places(1 place per entry and/or command) using a combination of push and unshift commands.  Using a for loop, add each place to the screen in its own p tag. 
+ * 
+ * * Berlin
+ * * Jakarta
+ * * Wellington
+ * * Kyiv
+ * * Accra
+ */
+
+/**
+ * ! Section 4: Functions - 12 points
+ * * Create a function that will take in 4 numbers, add them together, then console log the result.
+ */
+
+
